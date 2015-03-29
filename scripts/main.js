9c05a3e933f9ac83cdcffd28bddf6a0062a3cc3b
crotten <wildrunts@gmail.com>
2015-03-28 22:50:08 -0400

    added images and changed up the navigation and added new tables

--- scripts/main.js
+++ scripts/main.js
@@ -14,4 +14,15 @@ $(document).ready(function () {
       html:true
     });
 
+    $('.myMenuNew > li').bind('mouseover', openSubMenu);
+		$('.myMenuNew > li').bind('mouseout', closeSubMenu);
+
+		function openSubMenu() {
+			$(this).find('ul').css('visibility', 'visible');
+		};
+
+		function closeSubMenu() {
+			$(this).find('ul').css('visibility', 'hidden');
+		};
+
 });
